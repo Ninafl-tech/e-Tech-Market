1b68951ec6485d1615504e52d1b148720380683d
Nino Kshutashvili <qshutashvilinino@gmail.com>
2023-06-29 18:24:12 +0400

    initial setup for slider

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+export function Slider() {
+  return (
+    <div>
+      <Carousel
+        infiniteLoop={true}
+        autoPlay={true}
+        showStatus={false}
+        showArrows={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <h1>Hello</h1>
+          <img src="https://images.unsplash.com/photo-1593642532009-6ba71e22f468?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1738&q=80" />
+          {/* <p className="legend">Legend 1</p> */}
+        </div>
+        <div></div>
+      </Carousel>
+
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
