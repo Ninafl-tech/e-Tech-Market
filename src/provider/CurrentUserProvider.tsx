e7bee69913b6cf20b68eb11594e5d9f10fcaf797
Nino Kshutashvili <qshutashvilinino@gmail.com>
2023-07-05 22:50:28 +0400

    Protected routes

--- src/provider/CurrentUserProvider.tsx
+++ src/provider/CurrentUserProvider.tsx
@@ -6,59 +6,41 @@ import React, {
 } from "react";
 import axios from "axios";
 import { baseURL } from "../config/baseURL.config";
+import { Tlocalstorage } from "../types/TlocalStorage";
+import jwt_decode from "jwt-decode";
+import { T } from "styled-icons/fa-solid";
 
-export type TCurrentUserContext = {
-  currentUser: {
-    id: string;
-    firstName: string;
-    lastName: string;
-  };
-  setCurrentUser: React.Dispatch<
-    React.SetStateAction<{
-      id: string;
-      firstName: string;
-      lastName: string;
-    }>
-  >;
+export enum TUserTypes {
+  ADMIN = "ADMIN",
+  GUEST = "GUEST",
+  USER = "USER",
+}
+
+type TCurrentUserContext = {
+  currentUser: TUserTypes;
+  setCurrentUser: React.Dispatch<React.SetStateAction<TUserTypes>>;
 };
 
 export const CurrentUserContext = createContext<TCurrentUserContext>({
-  currentUser: {
-    id: "",
-    firstName: "",
-    lastName: "",
-  },
+  currentUser: TUserTypes.GUEST,
   setCurrentUser: () => {},
 });
 
-export function CurrentUserProvider({ children }: PropsWithChildren<{}>) {
-  const [currentUser, setCurrentUser] = useState<{
-    id: string;
-    firstName: string;
-    lastName: string;
-  }>({
-    id: "",
-    firstName: "",
-    lastName: "",
-  });
-  useEffect(() => {
-    const fetchData = async () => {
-      const accessToken = localStorage.getItem("AccessToken");
-      if (accessToken) {
-        try {
-          const resp = await axios.get(`${baseURL}/me`, {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          });
-          setCurrentUser(resp.data);
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    };
+export function CurrentUserProvider({ children }: PropsWithChildren) {
+  const [currentUser, setCurrentUser] = useState<TUserTypes>(TUserTypes.GUEST);
 
-    fetchData();
+  const accessToken = localStorage.getItem("AccessToken");
+
+  useEffect(() => {
+    if (accessToken) {
+      const { isAdmin }: { isAdmin: boolean } = jwt_decode(accessToken);
+      console.log(isAdmin);
+      if (isAdmin) {
+        setCurrentUser(TUserTypes.ADMIN);
+      } else {
+        setCurrentUser(TUserTypes.USER);
+      }
+    }
   }, []);
 
   return (
